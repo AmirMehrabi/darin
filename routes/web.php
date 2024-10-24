f0452de5d3058ed5f649f3de7125afab53d7bef1
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2024-10-24 13:22:56 +0330

    Fixed a minor issue

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::post('submit-form', function(Request $request){
 
 
         // Send the data to the API endpoint
-        $response = Http::post('http://localhost:8001/api/subscription-services/create', $data);
+        $response = Http::post('https://demo.wispgate.io/api/subscription-services/create', $data);
 
         // Handle the response as needed
         if ($response->successful()) {
